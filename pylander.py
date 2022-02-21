0bc029bb2ed3cdff3b02295e39d4f5c89ccc35a0
Drew Herron <herron@mailbox.org>
2022-02-21 00:28:21 -0800

    Cleanup

--- pylander.py
+++ pylander.py
@@ -2,14 +2,15 @@ import os
 import random
 import time
 
-battle_speed = 3
+# How long should each battle last?
+battle_length = 1
 
-# Populate list of all files in dir/subdirs
+# Your files feel an irresistible pull...
 def gathering():
     immortals = []
     for subdir, dirs, files in os.walk('./'):
         for file in files:
-            # Don't add this program
+            # This file is on holy ground
             if 'pylander' in file:
                 continue
             else:
@@ -23,14 +24,11 @@ time.sleep(1)
 print("There can be only one.")
 time.sleep(1)
 
-# Pick two random files
 def encounter(immortals):
     if len(immortals) > 1:
         fighters = random.sample(immortals, 2)
-        #print("Immortals: ", immortals)
-        #print("Fighters: ", fighters)
         print(fighters[0], "vs.", fighters[1])
-        time.sleep(battle_speed)
+        time.sleep(battle_length)
     return fighters
 
 def quickening(victor):
@@ -46,7 +44,7 @@ def duel(fighters):
     immortals.remove(immortals[fail])
     victor = fighters[0]
     quickening(victor)
-    time.sleep(battle_speed)
+    time.sleep(battle_length)
 
 immortals = gathering()
 while len(immortals) > 1:
