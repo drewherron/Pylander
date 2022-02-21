43a04b1045eff675cfe67c799f0b4f489098bdd1
Drew Herron <herron@mailbox.org>
2022-02-21 02:10:06 -0800

    Haggis? What is haggis?

--- pylander.py
+++ pylander.py
@@ -10,42 +10,51 @@ def gathering():
     immortals = []
     for subdir, dirs, files in os.walk('./'):
         for file in files:
-            # This file is on holy ground
+            # but this file is on holy ground
             if 'pylander' in file:
                 continue
             else:
                 immortals += [os.path.join(subdir,file)]
     return immortals
 
-print("Files loaded.")
-time.sleep(1)
+# What is happening?
+print()
+print("All files loaded.")
+time.sleep(.5)
 print("The Gathering is complete.")
 time.sleep(1)
-print("There can be only one.")
+print("There can be only one.\n")
 time.sleep(1)
 
+# Why does the sun come up?
 def encounter(immortals):
     if len(immortals) > 1:
         fighters = random.sample(immortals, 2)
-        print(fighters[0], "vs.", fighters[1])
+        print(fighters[0], " vs ", fighters[1])
         time.sleep(battle_length)
     return fighters
 
-def quickening(victor):
+# Or are the stars just pinholes in the curtain of night?
+def quickening(winner):
     if len(immortals) == 1:
-        print(immortals[0], "wins the Prize.")
+        print(winner, "wins the Prize.\n")
 
+# Who knows?
 def duel(fighters):
+    # It's all decided for us
     fail = random.randint(0,1)
-    print("fail: ", fail)
-    print(fighters[fail], "deleted.")
+    print(fighters[fail], "deleted.\n")
     os.remove(fighters[fail])
     fighters.remove(fighters[fail])
     immortals.remove(immortals[fail])
-    victor = fighters[0]
-    quickening(victor)
+    # This world has only
+    winner = fighters[0]
+    # one sweet moment
+    quickening(winner)
+    # set aside for us
     time.sleep(battle_length)
 
+# Who wants to live forever?
 immortals = gathering()
 while len(immortals) > 1:
     immortals = gathering()
